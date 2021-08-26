6926c1cb8a695e24b8545809c32ff9a11e55dc6e
Violet Stewart <vkazoo2@gmail.com>
2021-08-26 14:12:08 -0700

    added UI for subtract

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+import { add } from './calculator.js';
+
+
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
-console.log(addNumber1, addNumber2, addButton, addAnswer);
+//console.log(addNumber1, addNumber2, addButton, addAnswer);
 
 addButton.addEventListener('click', () => {
-    console.log('This button was clicked');
+    const num1 = Number(addNumber1.value);
+    const num2 = Number(addNumber2.value);
+
+    const sum = add(num1, num2);
+    addAnswer.textContent = sum;
+    
+    //console.log('This button was clicked');
 });
 
-// import functions
-   // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+

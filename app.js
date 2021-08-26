5e62329484cab73dd2696a4553052eec764f3f2f
Violet Stewart <vkazoo2@gmail.com>
2021-08-26 13:07:40 -0700

    created UI for add

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 // import functions
-
-// reference needed DOM elements
-
-
-// set event listeners 
-    // get user input(s)
+   // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)

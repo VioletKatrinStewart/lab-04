9b8757417b3082560057eb74cdd72ae08609fe37
Violet Stewart <vkazoo2@gmail.com>
2021-08-26 13:34:51 -0700

    failed add function test

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,20 @@
 // IMPORT MODULES under test here:
+import { add } from '../calculator.js';
+
+
+
+const test = QUnit.test;
+
+test('add function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 5;
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = add(2, 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});

749ca38680e935bfeac7db24b09f261ce9642003
Violet Stewart <vkazoo2@gmail.com>
2021-08-26 14:58:11 -0700

    made multiplication work

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { add } from './calculator.js';
 import { subtract } from './calculator.js';
+import { multiply } from './calculator.js';
 
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
@@ -10,6 +11,11 @@ const subtractNumber1 = document.getElementById('subtract-number-1');
 const subtractNumber2 = document.getElementById('subtract-number-2');
 const subtractButton = document.getElementById('subtract-button');
 const subtractAnswer = document.getElementById('subtract-answer');
+
+const multiplyNumber1 = document.getElementById('multiply-number-1');
+const multiplyNumber2 = document.getElementById('multiply-number-2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyAnswer = document.getElementById('multiply-answer');
 
 //console.log(addNumber1, addNumber2, addButton, addAnswer);
 
@@ -28,6 +34,13 @@ subtractButton.addEventListener('click', () => {
 
     const sum = subtract(num1, num2);
     subtractAnswer.textContent = sum;
+});
+multiplyButton.addEventListener('click', () => {
+    const num1 = Number(multiplyNumber1.value);
+    const num2 = Number(multiplyNumber2.value);
+
+    const sum = multiply(num1, num2);
+    multiplyAnswer.textContent = sum;
 });
 
 
--- calculator.js
+++ calculator.js
@@ -10,3 +10,7 @@ export function add(x, y) {
 export function subtract(x, y) {
     return x - y;
 }
+
+export function multiply(x, y) {
+    return x * y;
+}
